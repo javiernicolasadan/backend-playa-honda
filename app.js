e2d42eda7ab19d0588516a1c80acf2fc6dc4fd29
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2024-10-17 23:19:06 +0200

    SEO sitemap.xml

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const cors = require('cors');
 const express = require('express');
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { createGzip } = require('zlib');
 const app = express();
 const weatherRoutes = require('./routes/WeatherBack');
 require('dotenv').config();
@@ -7,6 +9,27 @@ require('dotenv').config();
 app.use(cors({
   origin: '*'
 }));
+
+app.get('/sitemap.xml', (req, res) => {
+  res.header('Content-Type', 'application/xml');
+  res.header('Content-Encoding', 'gzip');
+
+  const smStream = new SitemapStream({ hostname: 'https://playa-honda.vercel.app' });  // Cambia a tu dominio
+  const pipeline = smStream.pipe(createGzip());
+
+  // Aquí agregas las rutas que deseas incluir en el sitemap
+  smStream.write({ url: '/', changefreq: 'daily', priority: 1.0 });
+  smStream.write({ url: '/apartment', changefreq: 'weekly', priority: 0.8 });
+  smStream.write({ url: '/gallery', changefreq: 'weekly', priority: 0.8 });
+  smStream.write({ url: '/activities', changefreq: 'weekly', priority: 0.8 });
+  smStream.write({ url: '/booking', changefreq: 'weekly', priority: 0.8 });
+  smStream.write({ url: '/weather', changefreq: 'daily', priority: 0.9 });
+
+  smStream.end();  // Finalizamos el stream
+
+  // Enviamos el sitemap comprimido
+  streamToPromise(pipeline).then(sm => res.send(sm)).catch(console.error);
+});
 
 
 app.use('/api', weatherRoutes);
